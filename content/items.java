39c15fc2df1b3fb7e189c0b6464421d2cbba5d91
CalebMonkey <100516589+CalebMonkey@users.noreply.github.com>
2022-10-13 12:06:38 +0100

    Update items.java

--- content/items.java
+++ content/items.java
@@ -3,14 +3,24 @@ package mindustrytaller.content;
 import mindustry.type.item
 
 public class MTItems{
-  public static Item
-  graphene, nanohex, microcore, cryocube;
+ 	public static Item
+ 	graphene, nanohex, microcore, cryocube;
   
-  public static void load(){
+	public static void load(){
 		graphene = new Item("graphene", Color.valueOf("b2c6d2")){{
-	cost = 1.0f;
+			cost = 1.0f;
 		}};
-		
+
 		nanohex = new Item("nanohex", Colour.valueOf("5c5e9f")){{
-	cost = 1.0f;
+			cost = 1.0f;
 		}};
+
+		microcore = new Item("graphene", Color.valueOf("ffd27e")){{
+			cost = 1.0f;
+		}};
+
+		cryocube = new Item("nanohex", Colour.valueOf("7a8cdc")){{
+			cost = 1.0f;
+		}};
+	}
+}
