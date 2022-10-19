3809cc09a713e55978b4316adb8f798a7f4709b0
CalebMonkey <100516589+CalebMonkey@users.noreply.github.com>
2022-10-19 18:31:21 +0100

    Update blocks.java

--- content/blocks.java
+++ content/blocks.java
@@ -1,1 +1,8 @@
+package mindustrytaller.content;
 
+import mindustry.world.blocks.*;
+
+public class Blocks{
+    public static Block
+    metaglassCrucible, phaseExtrapolator, surgeForge, pyratiteMill, blastMill, oilPowerPlant, nanohexAssembler, microcoreConstructor;
+ 
