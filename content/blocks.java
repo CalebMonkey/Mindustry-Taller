7b8f79692e84bf1585e7c06db556a458ddf197d6
CalebMonkey <100516589+CalebMonkey@users.noreply.github.com>
2022-10-19 18:33:59 +0100

    Update blocks.java

--- content/blocks.java
+++ content/blocks.java
@@ -5,4 +5,6 @@ import mindustry.world.blocks.*;
 public class Blocks{
     public static Block
     metaglassCrucible, phaseExtrapolator, surgeForge, pyratiteMill, blastMill, oilPowerPlant, nanohexAssembler, microcoreConstructor;
- 
+    
+    
+    public static void load(){
