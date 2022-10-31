4432cad40bf40557ee62aa25f3007cc5f11b6fa4
CalebMonkey <100516589+CalebMonkey@users.noreply.github.com>
2022-10-31 10:52:39 +0000

    Update blocks.java

--- content/blocks.java
+++ content/blocks.java
@@ -34,6 +34,25 @@ public class Blocks{
             itemCapacity = 30;
             consumePower(4.5f);
             consumeItems(with(Items.graphite, 4, Items.sand, 6, Items.pyratite, 1));
+            outputItem = new ItemStack(Items.metaglass, 10)
+            
+        plastaniumCoagulator = new GenericCrafter("plastaniumCoagulator"){{
+            requirements(Category.crafting, with(Items.titanium, 200, Items.silicon, 160, Items.graphene, 40, Items.plastanium, 65));
+            size = 3;
+            hasPower = true;
+            hasItems = true;
+            hasLiquids = true;
+            craftEffect = Fx.smeltsmoke;
+            drawer = new DrawMulti(new DrawDefault(), new DrawFlame(Color.valueOf("ffc099")));
+            ambientSound = Sounds.smelter;
+            ambientSoundVolume = 0.07f;
+            craftTime = 30f;
+            itemCapacity = 30;
+            consumePower(6f);
+            consumeItems(with(Items.titanium, 3, Items.lead,3));
+            consumeLiquid(Liquids.oil, 0.75f);
+            outputItem = new ItemStack(Items.plastanium, 3);
+            
             
             
         
