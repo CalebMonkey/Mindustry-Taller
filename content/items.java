2c742aa4cbe5c704c2aafbf3ad77fb429b662483
CalebMonkey <100516589+CalebMonkey@users.noreply.github.com>
2023-01-06 12:52:44 +0000

    Update items.java

--- content/items.java
+++ content/items.java
@@ -40,9 +40,16 @@ public class MTItems{
 			charge = 0.6f;
 		}};
 
-		magnetite = new Item("magnetite", Color.valueOf("f15454")){{
-			cost = 1.2f;
-			charge = 0.6f;
+		fractalMatter = new Item("fractal-matter", Color.valueOf("00deff")){{
+			cost = 1.5f;
+			charge = 2.0f;
+			explosiveness = 10.0f;
+			radioactivity = 5.0;
 		}};
+		nanocore = new Item("nanocore", Color.valueOf("989aa4")){{
+			cost = 2.0f;
+			charge = 10.0f;
+			explosiveness = 10.0f;
+		
 	}
 }
