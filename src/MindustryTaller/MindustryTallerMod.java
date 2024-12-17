4ea17426d4ea9b75c970ea1d3cb830fcfbe53021
CalebMonkey <100516589+CalebMonkey@users.noreply.github.com>
2024-12-17 15:03:56 +0000

    Update MindustryTallerMod.java

--- src/MindustryTaller/MindustryTallerMod.java
+++ src/MindustryTaller/MindustryTallerMod.java
@@ -1,6 +1,8 @@
 
 package MindustryTaller;
 
+import arc.*;
+import arc.util.*;
 import mindustry.*;
 import mindustry.content.*;
 import mindustry.game.EventType.*;
@@ -8,14 +10,15 @@ import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
+
 import MindustryTaller.content.*;
 import MindustryTaller.content.blocks.*;
 import arc.*;
 import mindustry.game.EventType.FileTreeInitEvent;
 import mindustry.mod.Mod;
 
-public class mindustryTallerModLoader extends Mod {
-	public mindustryTallerModLoader() {
+public class MindustryTallerMod extends Mod {
+	public MindustryTallerMod() {
 		Events.on(FileTreeInitEvent.class, e -> Sfx.load());
 	}
 
